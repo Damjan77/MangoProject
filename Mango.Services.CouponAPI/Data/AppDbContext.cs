5393235f274c433ecb3c676b5106b9a91e054902
Damjan77 <80587900+Damjan77@users.noreply.github.com>
2025-12-03 21:36:37 +0100

    Added database CouponAPI

---
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Coupon> Coupons { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Models.Coupon>().HasData(
+                new Models.Coupon
+                {
+                    CouponId = 1,
+                    CouponCode = "10OFF",
+                    DiscountAmount = 10,
+                    MinAmount = 20
+                },
+                new Models.Coupon
+                {
+                    CouponId = 2,
+                    CouponCode = "20OFF",
+                    DiscountAmount = 20,
+                    MinAmount = 40
+                }
+            );
+        }
+    }
+}
--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<Mango.Services.CouponAPI.Data.AppDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,4 +28,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigration();
+
 app.Run();
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<Mango.Services.CouponAPI.Data.AppDbContext>();
+        
+        if(_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
